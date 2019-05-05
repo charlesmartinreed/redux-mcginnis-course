ade4eb726b14f5db7b5c98d7802799f8d1a80238
charlesmartinreed <charlesmartinreed@icloud.com>
2019-05-05 18:49:23 -0500

    Add action types for removing, toggling todo items. Refactor todos as a switch conditional.

--- index.js
+++ index.js
@@ -37,12 +37,36 @@
 // if state is undefined, set it to an empty array
 // reducer function - takes in state and action and reduces those into a brand new state
 function todos(state = [], action) {
-	if(action.type === 'ADD_TODO') {
-		// concat, not push. concat does not modify current state array, but rather returns a new array
-		return state.concat([action.todo])
-	}
-
-	return state
+  switch (action.type) {
+    case "ADD_TODO":
+      return state.concat([action.todo]);
+    case "REMOVE_TODO":
+      return state.filter(todo => todo.id !== action.id);
+    case "TOGGLE_TODO":
+      return state.map(todo =>
+        todo.id !== action.id
+          ? todo
+          : Object.assign({}, todo, { complete: !todo.complete })
+      );
+    default:
+      return state;
+  }
+  // if (action.type === "ADD_TODO") {
+  //   // concat, not push. concat does not modify current state array, but rather returns a new array
+  //   return state.concat([action.todo]);
+  // } else if (action.type === "REMOVE_TODO") {
+  //   // so we want to filter out the todo from our state that has a matching ID
+  //   return state.filter(todo => todo.id !== action.id);
+  // } else if (action.type === "TOGGLE_TODO") {
+  //   // because this is a pure function, we need to make sure that we are not modfiying the old object - object.assign to merge the old object and ALL of its properties with the new object
+  //   return state.map(todo =>
+  //     todo.id !== action.id
+  //       ? todo
+  //       : Object.assign({}, todo, { complete: !todo.complete })
+  //   );
+  // } else {
+  //   return state;
+  // }
 }
 
 function createStore(reducer) {
@@ -67,35 +91,35 @@ function createStore(reducer) {
 
   // 4. Update the state (PUBLIC API)
 
-	// establish strict rules on who can update the state - a collection of events that occur in our app that can change the state of our store
-	// we'll call these actions - objects which represent specfic event or action that occurs in our app that will evetnually change the state of our store
-	// ex: if action.type is ADD_TODO, add the todo to the state
+  // establish strict rules on who can update the state - a collection of events that occur in our app that can change the state of our store
+  // we'll call these actions - objects which represent specfic event or action that occurs in our app that will evetnually change the state of our store
+  // ex: if action.type is ADD_TODO, add the todo to the state
 
-	// needs to be a pure function
+  // needs to be a pure function
   // loop through listeners, invoke each function
 
-	const dispatch = (action) => {
-		// call our todos reducer function that gets us the new state
-		state = reducer(state, action)
+  const dispatch = action => {
+    // call our todos reducer function that gets us the new state
+    state = reducer(state, action);
 
-		// loop over listeners and invoke them
-		listeners.forEach((listener) => listener())
-	}
+    // loop over listeners and invoke them
+    listeners.forEach(listener => listener());
+  };
 
   // returns an object that represents the store
   return {
     getState,
     subscribe,
-		dispatch
+    dispatch
   };
 }
 
 const store = createStore(todos); //has getState, subscribe and dispatch methods
 store.dispatch({
-		type: 'ADD_TODO',
-		todo: {
-			id: 0,
-			name: 'Learn Redux',
-			complete: false
-		}
-}) // we dispatch ACTIONS to change the state of our application
+  type: "ADD_TODO",
+  todo: {
+    id: 0,
+    name: "Learn Redux",
+    complete: false
+  }
+}); // we dispatch ACTIONS to change the state of our application
