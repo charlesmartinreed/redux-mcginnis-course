881c70aa137551e65fae405a4cd4a26b47435b47
charlesmartinreed <charlesmartinreed@icloud.com>
2019-05-10 20:42:11 -0500

    Display answered and unanswered questions in the UI.

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,11 +3,62 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 
 // what data does this component need? authedUser's answers and unanswered questions
-
+// why component state? No benefit to sticking this state in the Redux store, works just fine here
 class Dashboard extends Component {
+  state = {
+    showAnswered: false
+  };
+
+  // toggle show answers methods
+  showUnanswered = () => {
+    this.setState(() => ({
+      showAnswered: false
+    }));
+  };
+
+  showAnswered = () => {
+    this.setState(() => ({
+      showAnswered: true
+    }));
+  };
+
   render() {
-    console.log(this.props);
-    return <div>Dashboard</div>;
+    const { showAnswered } = this.state;
+    const { answered, unanswered } = this.props;
+
+    const list = showAnswered === true ? answered : unanswered;
+
+    return (
+      <div>
+        <div className="dashboard-toggle">
+          <button
+            style={{
+              textDecoration: showAnswered === false ? "underline" : null,
+              outline: "none"
+            }}
+            onClick={this.showUnanswered}
+          >
+            Unanswered
+          </button>
+          <span> | </span>
+          <button
+            style={{
+              textDecoration: showAnswered === true ? "underline" : null,
+              outline: "none"
+            }}
+            onClick={this.showAnswered}
+          >
+            Answered
+          </button>
+        </div>
+
+        <ul className="dashboard-list">
+          {list.map(poll => (
+            <li key={poll.id}>{poll.question}</li>
+          ))}
+        </ul>
+      </div>
+    );
   }
 }
 
